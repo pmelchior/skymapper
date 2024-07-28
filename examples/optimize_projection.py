475bbbddd9b816735d423813e43ba6051abf1cc0
Peter Melchior <peter.m.melchior@gmail.com>
2024-07-27 21:31:40 -0400

    fixed coordinates in script; v0.4.5

--- examples/optimize_projection.py
+++ examples/optimize_projection.py
@@ -9,7 +9,7 @@ def getCatalog(size=10000, survey=None):
     xyz = np.random.normal(size=(size, 3))
     r = np.sqrt((xyz**2).sum(axis=1))
     dec = np.arccos(xyz[:,2]/r) / skm.DEG2RAD - 90
-    ra = - np.arctan2(xyz[:,0], xyz[:,1]) / skm.DEG2RAD
+    ra = 180 - np.arctan2(xyz[:,0], xyz[:,1]) / skm.DEG2RAD
 
     if survey is not None:
         inside = survey.contains(ra, dec)
@@ -35,16 +35,16 @@ if __name__ == "__main__":
         survey = TestSurvey()
     ra, dec = getCatalog(size, survey=survey)
 
-    # define the best WagnerIV projection for the footprint
+    # define the best Albers projection for the footprint
     # minimizing the variation in distortion, aka ellipticity
     for crit in [skm.meanDistortion, skm.maxDistortion, skm.stdDistortion]:
-        proj = skm.WagnerIV.optimize(ra, dec, crit)
+        proj = skm.Albers.optimize(ra, dec, crit)
         map = skm.Map(proj)
         map.grid()
-        #map.labelMeridianAtParallel(-90, meridians=[])
-        map.footprint(survey, nside=64, zorder=20, facecolor='w', alpha=0.3)
+        map.labelMeridiansAtParallel(0, meridians=[])
+        map.footprint(survey, nside=64, zorder=20, facecolor='k', alpha=0.5)
         a,b = proj.distortion(ra, dec)
-        c = map.extrapolate(ra, dec, 1-np.abs(b/a), vmin=0, vmax=0.3, resolution=72)
+        c = map.extrapolate(ra, dec, 1-np.abs(b/a), cmap='coolwarm', vmin=0, vmax=0.3, resolution=72)
         cb = map.colorbar(c, cb_label='distortion')
         map.focus(ra, dec)
         map.title(proj.__class__.__name__ + ": " + crit.__name__)
